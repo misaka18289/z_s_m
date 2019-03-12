e6f9663a0325813ef4fc9e71c46c590d7b680607
misaka17777 <misaka18289@outlook.com>
2019-03-12 22:27:38 +0800

    hello world again

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,3 @@ from flask import request
 @app.route('/index')
 def index():
     return request.args.get('text')
-
-@app.route('/cat')
-def cat():
-    num=int(request.args.get('num'))
-    return str(num+100)
